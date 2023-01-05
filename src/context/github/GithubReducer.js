acca7427a62358a9d05819a7710e28b50f6578bf
NABEEL KHAN <nabeelsk01@gmail.com>
2023-01-05 18:40:38 +0530

    First Commit

---
+++ src/context/github/GithubReducer.js
@@ -0,0 +1,31 @@
+const githubReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_LOADING':
+            return {
+                ...state,
+                loading: true,
+            }
+        case 'GET_USERS':
+            return {
+                ...state,
+                users: action.payload,
+                loading: false,
+            }
+        case 'GET_USER_AND_REPOS':
+            return {
+                ...state,
+                user: action.payload,
+                repos: action.payload,
+                loading: false,
+            }
+        case 'CLEAR_USERS':
+            return {
+                ...state,
+                users: [],
+            }
+        default:
+            return state
+    }
+}
+
+export default githubReducer
